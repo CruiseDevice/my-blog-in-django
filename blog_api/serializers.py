1aae7c40d1aa672dd6f71143633256358693c4e7
CruiseDevice <meakashchavan@gmail.com>
2019-10-01 19:30:11 +0530

    Show comments along with post in blog_api

--- blog_api/serializers.py
+++ blog_api/serializers.py
@@ -16,11 +16,8 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         fields = '__all__'
 
 class PostSerializer(serializers.ModelSerializer):
+    comments = serializers.StringRelatedField(many=True)
     class Meta:
         model = Post
-        fields = '__all__'
-
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = '__all__'
+        fields = ['author', 'slug', 'title', 'text', 'created_date',
+                 'published_date', 'status', 'comments']
