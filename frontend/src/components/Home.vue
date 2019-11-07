958d6088981f63012267e5fb709f44d6e87bc479
CruiseDevice <meakashchavan@gmail.com>
2019-11-07 17:31:52 +0530

    Blog Home Page.

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <center>
+      <div class="row">
+        <div class="card mb-3">
+          <img class="card-img-top rounded-circle" src="./../../static/img/10258150.png" alt="cruisedevice">
+          <div class="card-body">
+            <h3 class="card-title">Akash's Blog</h3>
+            <p class="card-text"><small class="text-muted">Developer</small></p>
+            <div class="social-links">
+              <a href="https://www.linkedin.com/in/iakashchavan/">Linkedin</a>
+              <a href="https://github.com/CruiseDevice">Github</a>
+              <a href="https://twitter.com/CruiseDevice">Twitter</a>
+            </div>
+          </div>
+        </div>      
+      </div>
+    </center>
+  </div>
+</template>
+
+<style scoped>
+
+.row{
+  display: flex-shrink;
+}
+
+img {
+  width: 10rem;
+  height: 1 0rem;
+}
+.card {
+  width: 40rem;
+  margin: 0 auto;
+  float: none;
+  margin-bottom: 10px;
+  margin-top: 1em;
+}
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '@/components/Home'
 import BlogList from '@/components/BlogList'
 
 Vue.use(Router)
@@ -7,6 +8,11 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
     {
       path: '/',
       name: 'BlogList',
