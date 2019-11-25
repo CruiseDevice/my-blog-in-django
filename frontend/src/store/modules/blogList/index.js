0aab1887d25c595f3edf9019a2ac51792dfe16fa
CruiseDevice <meakashchavan@gmail.com>
2019-11-26 00:40:50 +0530

    Show blogposts on Home page component

--- frontend/src/store/modules/blogList/index.js
+++ frontend/src/store/modules/blogList/index.js
@@ -2,31 +2,33 @@
 import axios from 'axios'
 
 const state = {
-
+  posts: []
 }
 
 const mutations = {
-
+  UPDATE_BLOG_POSTS(state, payload) {
+    state.posts = payload
+  }
 }
 
 const actions = {
-  fetchblogs ({commit}) {
+  fetchblogs (state) {
     axios({
         method: 'get',
         url: 'http://127.0.0.1:8000/api/',
         headers: {
-            // Authorization: 'Token ' + TOKEN,
             'Content-Type': 'application/json'
         }
     })
     .then((response) => {
-        console.log(response)
+        console.log(response.data)
+        state.commit('UPDATE_BLOG_POSTS', response.data)
     })
   }
 }
 
 const getters = {
-
+  getposts: state => state.posts
 }
 
 const blogListModule = {
