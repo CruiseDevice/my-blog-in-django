691300dba08eeca4954447094ffbd37df8a1aa97
CruiseDevice <meakashchavan@gmail.com>
2020-02-05 20:56:37 +0530

    Browse individual post in API

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
+from django.views.decorators.csrf import csrf_exempt
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list),
     url(r'^tag/(?P<tag_slug>[-\w]+)/$', views.post_list),
-    url(r'(?P<pk>\d+)/$', views.post_detail),
+    url(r'post/(?P<pk>\d+)/$', views.post_detail),
     url(r'drafts/$', views.post_draft),
 ]
